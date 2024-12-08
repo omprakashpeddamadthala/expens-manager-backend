c10589fbb9959098ce3678ca4280eeed75f56d7b
omprakashpeddamadthala <omprakashpeddamadthala@hotmail.com>
2024-12-08 21:39:13 +0530

    initial commit

---
+++ src/main/java/com/expens/manager/ExpensManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.expens.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensManagerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( ExpensManagerBackendApplication.class, args );
+    }
+
+}
---
+++ src/test/java/com/expens/manager/ExpensManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expens.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensManagerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
