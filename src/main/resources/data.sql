02c1f749b1acd5a1e6b07b983a67ed653e49a4d9
Omprakash Peddamadthala <oppeddamadthala@cgsinc.com>
2024-12-12 19:50:59 +0530

    added swagger

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO tbl_expense (expense_id, name, note, category, date, amount, created_at, updated_at)
 VALUES
-    ('EXP001', 'Test Expense', 'This is a test expense', 'Food', '2024-12-10', 100.00, '2024-12-10 00:00:00', '2024-12-10 00:00:00');
+    (UUID(), 'Test Expense', 'This is a test expense', 'Food', '2024-12-10', 100.00, '2024-12-10 00:00:00', '2024-12-10 00:00:00');
 
 INSERT INTO tbl_expense (expense_id, name, note, category, date, amount, created_at, updated_at)
 VALUES
-    ('EXP002', 'Another Test Expense', 'This is another test expense', 'Transportation', '2024-12-11', 75.50, '2024-12-11 00:00:00', '2024-12-11 00:00:00');
+    (UUID(), 'Another Test Expense', 'This is another test expense', 'Transportation', '2024-12-11', 75.50, '2024-12-11 00:00:00', '2024-12-11 00:00:00');
