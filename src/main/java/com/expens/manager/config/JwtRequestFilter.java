f6887c4bccc42dc2e769501b8bae3f0a53dbccf1
omprakashpeddamadthala <omprakashpeddamadthala@hotmail.com>
2025-01-05 20:31:33 +0530

    completed logout api

--- src/main/java/com/expens/manager/config/JwtRequestFilter.java
+++ src/main/java/com/expens/manager/config/JwtRequestFilter.java
@@ -1,7 +1,7 @@
 package com.expens.manager.config;
 
-
 import com.expens.manager.service.CustomUserDetailsService;
+import com.expens.manager.service.impl.TokenBlackListService;
 import com.expens.manager.utils.JwtTokenUtil;
 import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
@@ -20,45 +20,73 @@ import java.io.IOException;
 public class JwtRequestFilter extends OncePerRequestFilter {
 
     @Autowired
-    private  JwtTokenUtil jwtTokenUtil;
+    private JwtTokenUtil jwtTokenUtil;
 
     @Autowired
     private CustomUserDetailsService userDetailsService;
 
+    @Autowired
+    private TokenBlackListService tokenBlackListService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        final String requestTokenHeader =  request.getHeader( "Authorization" );
-
-        String jwtToken = null;
-        String email = null;
-
-        if (request.getServletPath().equals("/login") || request.getServletPath().equals("/register") ){
+        if (shouldSkipFilter(request)) {
             filterChain.doFilter(request, response);
             return;
         }
 
-        if(requestTokenHeader != null && requestTokenHeader.startsWith( "Bearer" )){
-            jwtToken = requestTokenHeader.substring( 7 );
-            try{
-                email =   jwtTokenUtil.getUsernameFromToken(jwtToken );
-            }catch (IllegalArgumentException exception){
-                throw new RuntimeException("Unable to get jwt token");
-            }catch (ExpiredJwtException exception){
-                throw new RuntimeException("Jwt token has expired");
-            }
-
-            if(email!=null && SecurityContextHolder.getContext().getAuthentication() ==null){
-                UserDetails userDetails = userDetailsService.loadUserByUsername( email );
-
-                if(jwtTokenUtil.validateToken( jwtToken,userDetails )){
-                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken( userDetails,null,userDetails.getAuthorities());
-                    authenticationToken.setDetails( new WebAuthenticationDetailsSource().buildDetails( request ) );
-                    SecurityContextHolder.getContext().setAuthentication( authenticationToken );
-                }
-            }
-            filterChain.doFilter( request,response );
-        }else {
-            throw new  RuntimeException("Jwt token does not begin with Bearer String");
+        String jwtToken = extractTokenFromHeader(request);
+        if ( isValidToken(jwtToken)) {
+            handleInvalidToken(response);
+            return;
         }
+
+        String email =   jwtTokenUtil.getUsernameFromToken(jwtToken );
+        UserDetails userDetails = loadUserDetails(email);
+        if (!validateToken(jwtToken, userDetails)) {
+            throw new RuntimeException("Unable to validate JWT token");
+        }
+
+        authenticateUser(request, userDetails);
+
+        filterChain.doFilter(request, response);
+    }
+
+    private boolean shouldSkipFilter(HttpServletRequest request) {
+        return request.getServletPath().equals("/login") || request.getServletPath().equals("/register");
+    }
+
+    private String extractTokenFromHeader(HttpServletRequest request) {
+        String requestTokenHeader = request.getHeader("Authorization");
+        return requestTokenHeader != null && requestTokenHeader.startsWith("Bearer ") ? requestTokenHeader.substring(7) : null;
+    }
+
+    private boolean isValidToken(String jwtToken) {
+        return jwtToken != null && tokenBlackListService.isTokenIsBlockListed(jwtToken);
+    }
+
+    private void handleInvalidToken(HttpServletResponse response) throws IOException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid token");
+    }
+
+    private UserDetails loadUserDetails(String email) {
+        return userDetailsService.loadUserByUsername(email);
+    }
+
+    private boolean validateToken(String jwtToken, UserDetails userDetails) {
+        try {
+            return jwtTokenUtil.validateToken(jwtToken, userDetails);
+        } catch (IllegalArgumentException e) {
+            throw new RuntimeException("Unable to get JWT token", e);
+        } catch (ExpiredJwtException e) {
+            throw new RuntimeException("JWT token has expired", e);
+        }
+    }
+
+    private void authenticateUser(HttpServletRequest request, UserDetails userDetails) {
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                userDetails, null, userDetails.getAuthorities());
+        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
     }
 }
